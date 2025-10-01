9a837cf672f2571ffac9a8ac323ecc01b4abd21f
yuuhikaze <yuuhikaze@protonmail.com>
2025-09-30 20:22:23 -0500

    refactor: removes rushed TDA implementation code

--- src/main/java/com/yuuhikaze/ed202510/TDA/UnsortedPriorityQueue.java
+++
@@ -1,6 +0,0 @@
-package com.yuuhikaze.ed202510.TDA;
-
-public class UnsortedPriorityQueue<K extends Comparable<K>,V> extends AbstractPriorityQueue<K,V> {
-
-    // private 
-}
--- src/main/java/com/yuuhikaze/ed202510/TDA/interfaces/Position.java
+++
@@ -1,6 +0,0 @@
-package com.yuuhikaze.ed202510.TDA.interfaces;
-
-public interface Position<E> {
-
-    E getElement() throws IllegalStateException;
-}
